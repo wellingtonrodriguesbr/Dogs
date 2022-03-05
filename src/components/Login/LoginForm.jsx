5c973eaed90cc800e6c7882147656f2c9e34db4b
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-03-05 12:01:17 -0300

    style: add button and error message

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -6,6 +6,10 @@ import useForm from "../../hooks/useForm";
 
 import Button from "../Forms/Button/Button";
 import Input from "../Forms/Input/Input";
+import Error from "../Helper/Error";
+
+import styles from "../../styles/LoginForm.module.css";
+import stylesBtn from "../Forms/Button/Button.module.css";
 
 export default function LoginForm() {
   const { userLogin, error, loading } = useContext(UserContext);
@@ -21,9 +25,9 @@ export default function LoginForm() {
   }
 
   return (
-    <section>
-      <h1>Login</h1>
-      <form action="" onSubmit={handleSubmit}>
+    <section className="animatedEntry">
+      <h1 className="title">Login</h1>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <Input label="Usúario" type="text" name="username" {...username} />
         <Input label="Senha" type="password" name="password" {...password} />
         {loading ? (
@@ -32,9 +36,18 @@ export default function LoginForm() {
           <Button>Fazer login</Button>
         )}
 
-        {error && <p>{error}</p>}
+        <Error error={error} />
       </form>
-      <Link to="/login/create">Cadastro</Link>
+      <Link className={styles.forgot} to="/login/forgotPassword">
+        Esqueceu sua senha?
+      </Link>
+      <div className={styles.register}>
+        <h2 className={styles.subtitle}>Cadastre-se</h2>
+        <p>Ainda não possui conta? Cadastre-se agora mesmo.</p>
+        <Link className={stylesBtn.button} to="/login/create">
+          Cadastrar-me
+        </Link>
+      </div>
     </section>
   );
 }
