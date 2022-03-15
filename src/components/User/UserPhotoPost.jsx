e814a0c30d3a6104b1d0c66a25c7fcf649fb7488
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-03-15 07:33:36 -0300

    refactor: update imports

--- src/components/User/UserPhotoPost.jsx
+++ src/components/User/UserPhotoPost.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import useFetch from "../../hooks/useFetch";
 import useForm from "../../hooks/useForm";
