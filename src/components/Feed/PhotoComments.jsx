ac95356061e7cca526aa97c7f5ef8ac6dbe6cc99
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-03-16 07:56:53 -0300

    chore: scroll automatic

--- src/components/Feed/PhotoComments.jsx
+++ src/components/Feed/PhotoComments.jsx
@@ -1,15 +1,20 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { UserContext } from "../../contexts/UserContext";
 import styles from "./PhotoComments.module.css";
 import PhotoCommentsForm from "./PhotoCommentsForm";
 
 export default function PhotoComments({ comments, id }) {
   const [commentsAll, setCommentsAll] = useState(() => comments);
+  const commentsSection = useRef(null);
   const { login } = useContext(UserContext);
+
+  useEffect(() => {
+    commentsSection.current.scrollTop = commentsSection.current.scrollHeight;
+  }, [commentsAll]);
 
   return (
     <>
-      <ul className={styles.comments}>
+      <ul ref={commentsSection} className={styles.comments}>
         {commentsAll.map((comment) => (
           <li key={comment.comment_ID}>
             <b>{comment.comment_author}: </b>
