7b72f3112120c75ce822b6427bafdf90f855ec79
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-03-17 06:59:42 -0300

    chore: skeleton img

---
+++ src/components/Helper/Image.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import styles from "./Image.module.css";
+
+export default function Image({ alt, ...props }) {
+  const [skeleton, setSkeleton] = useState(true);
+
+  function handleLoad({ target }) {
+    target.style.opacity = 1;
+    setSkeleton(false);
+  }
+
+  return (
+    <div className={styles.wrapper}>
+      {skeleton && <div className={styles.skeleton}></div>}
+      <img onLoad={handleLoad} className={styles.img} alt={alt} {...props} />
+    </div>
+  );
+}
