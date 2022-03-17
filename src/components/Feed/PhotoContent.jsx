ecaad370a764b4682f49e0825b5cf2234f650aef
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-03-17 07:01:38 -0300

    feat: skeleton img

--- src/components/Feed/PhotoContent.jsx
+++ src/components/Feed/PhotoContent.jsx
@@ -1,18 +1,29 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { UserContext } from "../../contexts/UserContext";
+import Image from "../Helper/Image";
 import PhotoComments from "./PhotoComments";
 import styles from "./PhotoContent.module.css";
+import PhotoDelete from "./PhotoDelete";
 
 export default function PhotoContent({ data }) {
+  const user = useContext(UserContext);
   const { photo, comments } = data;
+
   return (
     <div className={styles.photo}>
       <div className={styles.img}>
-        <img src={photo.src} alt={photo.title} />
+        <Image src={photo.src} alt={photo.title} />
       </div>
       <div className={styles.detailsPhoto}>
         <div>
           <p className={styles.author}>
-            <Link to={`/profile/${photo.author}`}>@{photo.author}</Link>
+            {user.data && user.data.username === photo.author ? (
+              <PhotoDelete id={photo.id} />
+            ) : (
+              <Link to={`/profile/${photo.author}`}>@{photo.author}</Link>
+            )}
+
             <span className={styles.views}>{photo.acessos}</span>
           </p>
           <h1 className="title">
