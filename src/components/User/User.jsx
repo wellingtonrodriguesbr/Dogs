5a5653cddff7221ca20c050fb2c3c5dffedef13a
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-03-18 07:05:08 -0300

    chore: add props

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,15 +1,18 @@
+import { useContext } from "react";
 import { Route, Routes } from "react-router-dom";
+import { UserContext } from "../../contexts/UserContext";
 import Feed from "../Feed/Feed";
 import UserHeader from "./UserHeader";
 import UserPhotoPost from "./UserPhotoPost";
 import UserStatistics from "./UserStatistics";
 
 export default function User() {
+  const { data } = useContext(UserContext);
   return (
     <section className="container">
       <UserHeader />
       <Routes>
-        <Route path="/" element={<Feed />} />
+        <Route path="/" element={<Feed user={data.id} />} />
         <Route path="new/post" element={<UserPhotoPost />} />
         <Route path="statistics" element={<UserStatistics />} />
       </Routes>
